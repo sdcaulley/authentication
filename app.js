3f3b37802ab66e7894da5cb0a63a66c76a1b910c
sdcaulley <stephanie.caulley@gmail.com>
2019-08-19 15:28:09 -0500

    set up basic routes for auth

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const { port } = require('./config.js');
+const authRoutes = require('./routes/auth-routes');
 
 //set up view engine
 app.set('view engine', 'ejs');
+
+//set up authRoutes
+app.use('/auth', authRoutes);
 
 //create home route
 app.get('/', (req, res) => {
