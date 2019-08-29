60ca490c09dc116efb4c11df1d93539c9113df1f
sdcaulley <stephanie.caulley@gmail.com>
2019-08-29 14:57:40 -0500

    clean up of old attempts

--- config/passport-setup.js
+++
@@ -1,8 +0,0 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth');
-
-passport.use(new GoogleStrategy({
-  //options for the google strategy
-}), () => {
-  //passport callback function
-});
